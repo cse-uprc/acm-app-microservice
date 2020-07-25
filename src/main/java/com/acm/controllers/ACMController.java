ac11424ccae51b1ec84b43891273802851d1f179
Samuel Butler <sambutler1017@icloud.com>
2020-07-24 22:44:46 -0400

    Work

--- src/main/java/com/acm/controllers/ACMController.java
+++ src/main/java/com/acm/controllers/ACMController.java
@@ -15,8 +15,6 @@ public class ACMController {
 	/**
 	 * End point to create a manager based on the given request
 	 * 
-	 * @param manager - manager details to create
-	 * @return Manager Detail Object {@link ManagerDetail}
 	 * @since July 11, 2020
 	 */
 	@GetMapping()
