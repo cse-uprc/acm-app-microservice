83dfe75440f58159c3cf0cdea67478026309fa0a
fluebee <33874085+fluebee@users.noreply.github.com>
2020-08-01 16:58:42 -0400

    Updated library jar (#65)

--- src/main/java/com/acm/app/user/rest/UserController.java
+++ src/main/java/com/acm/app/user/rest/UserController.java
@@ -1,15 +1,13 @@
 package com.acm.app.user.rest;
 
-import com.acm.app.user.client.domain.request.UserGetRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.acm.app.user.client.domain.User;
+import com.acm.app.user.client.domain.request.UserGetRequest;
 import com.acm.app.user.service.UserService;
 
 @CrossOrigin
