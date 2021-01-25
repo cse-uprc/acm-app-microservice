dd342b6ed8334cbdb78bc1cc79a9c38c158af87f
DESKTOP-0MRSA9E\sambu <sambutler1017@icloud.com>
2021-01-24 21:52:46 -0500

    Updating email settings

--- src/main/java/com/acm/app/mail/service/MailService.java
+++ src/main/java/com/acm/app/mail/service/MailService.java
@@ -95,9 +95,10 @@ public class MailService {
     private Properties getProperties() {
         Properties props = new Properties();
 
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.from", "acm.email.website@gmail.com");
+        props.put("mail.from", "acm.web@outlook.com");
+        props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp-mail.outlook.com");
         props.put("mail.smtp.port", "587");
         props.setProperty("mail.debug", "true");
 
